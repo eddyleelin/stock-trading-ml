31bda7130cde46a80b82a255991ac64a17763f40
Edward Lin <ell22@duke.edu>
2020-07-02 20:40:37 -0400

    Revert "Update save_data_to_csv.py"
    This reverts commit 826037c09d0304272920c3d98dac4417384ceb2c.

--- save_data_to_csv.py
+++ save_data_to_csv.py
@@ -5,8 +5,8 @@ import argparse
 
 
 def save_dataset(symbol, time_window):
-    # credentials = json.load(open('creds.json', 'r'))
-    api_key = "XQRL2QJZULZA0TFU"
+    credentials = json.load(open('creds.json', 'r'))
+    api_key = credentials['av_api_key']
     print(symbol, time_window)
     ts = TimeSeries(key=api_key, output_format='pandas')
     if time_window == 'intraday':
